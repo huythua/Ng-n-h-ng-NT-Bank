55e9ef71e4b59c0c4aa18e949ad5bfd90d7c1353
huythua <110289075+huythua@users.noreply.github.com>
2022-08-22 23:25:03 +0700

    upp

---
+++ tygia.js
@@ -0,0 +1,6 @@
+var d = new Date();
+
+document.getElementById("date").innerHTML = "Hôm nay, " + "Ngày " + d.getDate() + " Tháng " + ((d.getMonth())+1) +" Năm "+ d.getFullYear();
+
+
+document.getElementById("dates").innerHTML = "Hôm nay, " + "Ngày " + d.getDate() + " Tháng " + ((d.getMonth())+1) +" Năm "+ d.getFullYear();
