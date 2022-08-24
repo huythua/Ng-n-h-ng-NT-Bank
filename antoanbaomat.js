c83ec58cb0add5a1d5987280a1dbeaa2b47234f5
NHNgocNhu <2151053045nhu@ou.edu.vn>
2022-08-24 15:50:09 +0700

    hi

--- antoanbaomat.js
+++ antoanbaomat.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $('.accordion-item.active .accordion-body').slideDown();
 $('.accordion-header').click(function(){
-    $(this).parent().toggleClass('active');
+    $(this).parent().toggleClass('active');  /* luân phiên hiển thị */
     $(this).parent().children('.accordion-body').slideToggle();
 });
 });
--- main.js
+++ main.js
@@ -25,8 +25,62 @@ if(hien !=""){
         'success'
       )
     }else{
-
+        Swal.fire({     
+            title: 'Oops...',
+            text: 'Bạn Chưa Nhập Số Điện Thoại / Tên Đăng Nhập',
+           
+          })
     }
 }
 
-
+function enter(){
+    var save= document.getElementsByClassName("form-input")[0].value;
+    if(save !=""){
+        Swal.fire({
+            title: 'Do You Want To Save The Passwords?',
+            showDenyButton: true,
+            showCancelButton: true,
+            confirmButtonText: 'Save',
+            denyButtonText: `Don't save`,
+          }).then((result) => {
+            /* Read more about isConfirmed, isDenied below */
+            if (result.isConfirmed) {
+              Swal.fire('Saved!', '', 'success')
+            } else if (result.isDenied) {
+              Swal.fire('Password is not saved!', '', 'info')
+            }
+          })
+        }else{
+            Swal.fire({     
+                title: 'Oops...',
+                text: 'Bạn Chưa Nhập Số Điện Thoại / Tên Đăng Nhập',
+               
+              })
+        }
+    }
+    function enter(){
+        var save= document.getElementsByClassName("form-input")[1].value;
+        var save1= document.getElementsByClassName("form-input")[0].value;
+        if(save !="" && save1!="" ){
+            Swal.fire({
+                title: 'Do You Want To Save The Passwords?',
+                showDenyButton: true,
+                showCancelButton: true,
+                confirmButtonText: 'Save',
+                denyButtonText: `Don't save`,
+              }).then((result) => {
+                /* Read more about isConfirmed, isDenied below */
+                if (result.isConfirmed) {
+                  Swal.fire('Saved!', '', 'success')
+                } else if (result.isDenied) {
+                  Swal.fire('Password is not saved!', '', 'info')
+                }
+              })
+            }else{
+                Swal.fire({     
+                    title: 'Oops...',
+                    text: 'Bạn Chưa Nhập Mật Khẩu / Số Điện Thoại!',
+                   
+                  })
+            }
+        }
